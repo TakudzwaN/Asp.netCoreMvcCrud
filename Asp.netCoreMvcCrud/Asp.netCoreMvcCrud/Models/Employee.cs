cb97d7f4d0b16455d964c9f5a3940890febe69e4
TakudzwaN <TakudzwaN@users.noreply.github.com>
2019-05-11 18:01:50 +0200

    Added Employee Model

---
+++ Asp.netCoreMvcCrud/Asp.netCoreMvcCrud/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.netCoreMvcCrud.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string EmpCode { get; set; }
+
+        public string Position { get; set; }
+
+        public string OfficeLocation { get; set; }
+
+    }
+}
